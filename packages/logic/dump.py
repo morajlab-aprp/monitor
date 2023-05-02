6de6161cb79ea619c1ac41ecc5a670e67fbc956c
morteza-jamali <mortezajamali4241@gmail.com>
2023-05-02 04:43:31 -0300

    fix(packages): fix regex in `dump.py` script

--- packages/logic/dump.py
+++ packages/logic/dump.py
@@ -3,49 +3,50 @@ import service
 import re
 
 BatteryDumpType = {
-  "present": bool,
-  "ac powered": bool,
-  "usb powered": bool,
-  "wireless powered": bool,
-  "max charging current": int,
-  "max charging voltage": int,
-  "charge counter": int,
-  "temperature": int,
-  "voltage": int,
-  "status": int,
-  "health": int,
-  "level": int,
-  "scale": int,
-  "technology": str
+    "present": bool,
+    "ac powered": bool,
+    "usb powered": bool,
+    "wireless powered": bool,
+    "max charging current": int,
+    "max charging voltage": int,
+    "charge counter": int,
+    "temperature": int,
+    "voltage": int,
+    "status": int,
+    "health": int,
+    "level": int,
+    "scale": int,
+    "technology": str,
 }
 
+
 class Dump:
-  def __init__(self, value):
-    self.value = value
+    def __init__(self, value):
+        self.value = value
 
-  def getValue(self, key):
-    match = re.search(f"{key}:\s*(\w|\d)+", self.value, re.I)
+    def getValue(self, key):
+        match = re.search(f"{key}:\s*.+", self.value, re.I)
 
-    return match.group().split(':')[1].strip() if match else None
+        return match.group().split(":")[1].strip() if match else None
 
 
 def dumpService(name):
-  result = subprocess.call(["dumpsys", name])
+    result = subprocess.call(["dumpsys", name])
 
-  return Dump(result)
+    return Dump(result)
 
 
 def initObject(dump, interface):
-  result = {}
+    result = {}
 
-  for key, ttype in interface.items():
-    value = dump.getValue(key)
-    result[key] = None if value is None else ttype(value)
+    for key, ttype in interface.items():
+        value = dump.getValue(key)
+        result[key] = None if value is None else ttype(value)
 
-  return result
+    return result
 
 
 def getBatteryDump():
-  dump = dumpService(service.Battery)
+    dump = dumpService(service.Battery)
 
-  return initObject(dump, BatteryDumpType)
+    return initObject(dump, BatteryDumpType)
